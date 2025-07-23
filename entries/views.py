beb725fb3fc90bece34e795a8f862e92d6c6d12c
YoungSang Yun <saegyul@gmail.com>
2025-07-23 01:11:20 -0700

    This is the initial commit for start up new diary project.

---
+++ entries/views.py
@@ -0,0 +1,17 @@
+#from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    DetailView,
+)
+
+from .models import Entry
+
+class EntryListView(ListView):
+    model = Entry
+    queryset = Entry.objects.all().order_by("-date_created")
+
+class EntryDetailView(DetailView):
+    model = Entry
+
+
+# Create your views here.
